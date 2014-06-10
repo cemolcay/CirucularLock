9a4ee58ef5984a204925ca07270fba87fbe0e97d
cemolcay <ccemolcay@gmail.com>
2014-06-10 18:44:01 +0300

    Initial Commit

---
+++ CirucularLock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CirucularLock
+//
+//  Created by Cem Olcay on 10/06/14.
+//  Copyright (c) 2014 studionord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CirucularLock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CirucularLock
+//
+//  Created by Cem Olcay on 10/06/14.
+//  Copyright (c) 2014 studionord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
